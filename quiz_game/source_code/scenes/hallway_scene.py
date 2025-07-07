85597a8e321a8289fb8cc9f18532e6fef27c7387
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-07-08 00:03:14 +0800

    Imported necessities + loaded hallway scene background image

--- quiz_game/source_code/scenes/hallway_scene.py
+++ quiz_game/source_code/scenes/hallway_scene.py
@@ -1,6 +1,13 @@
 # scenes/hallway_scene.py
 
+import pygame
+from settings import ASSET_PATH, WIDTH, HEIGHT
+
 class HallwayScene:
     def __init__(self, game):
         self.game = game
         self.screen = game.screen
+
+        self.background = pygame.transform.scale(
+            pygame.image.load(ASSET_PATH + "school_hall_background.jpg"), (WIDTH, HEIGHT)
+        )
