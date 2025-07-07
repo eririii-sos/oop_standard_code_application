050723318f0c1a71d5e403367ba9abc9d9864b1a
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-07-07 23:44:40 +0800

    Declared new class for Developer Info. + Initialized objects

--- quiz_game/source_code/scenes/developer_info_scene.py
+++ quiz_game/source_code/scenes/developer_info_scene.py
@@ -2,3 +2,9 @@
 
 import pygame
 from settings import TEXT_FONT_SIZE
+
+class DeveloperInfoScene:
+    def __init__(self, game):
+        self.game = game
+        self.screen = game.screen
+        self.font = pygame.font.Font(None, TEXT_FONT_SIZE)
