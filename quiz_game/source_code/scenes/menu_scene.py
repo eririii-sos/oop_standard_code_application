a39bc035f875144470b049179906939199e13430
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-07-07 23:21:35 +0800

    Added the menu buttons

--- quiz_game/source_code/scenes/menu_scene.py
+++ quiz_game/source_code/scenes/menu_scene.py
@@ -12,3 +12,6 @@ class MenuScene:
             pygame.image.load(ASSET_PATH + "home_background.png"), (WIDTH, HEIGHT)
         )
         self.font = pygame.font.Font(None, TEXT_FONT_SIZE)
+        self.start_button = pygame.Rect(395, 270, 280, 60)
+        self.exit_button = pygame.Rect(395, 360, 280, 60)
+        self.info_button = pygame.Rect(WIDTH - 60, HEIGHT - 60, 40, 40)
