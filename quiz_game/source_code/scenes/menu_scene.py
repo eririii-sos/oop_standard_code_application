97bb924454b75b747f0cf767bc3f8483ad1d6d28
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-07-07 23:17:30 +0800

    Declared new class for menu scene + loaded menu/home background

--- quiz_game/source_code/scenes/menu_scene.py
+++ quiz_game/source_code/scenes/menu_scene.py
@@ -3,3 +3,11 @@
 import pygame
 from settings import WIDTH, HEIGHT, TEXT_FONT_SIZE, ASSET_PATH
 from core.music import play_music
+
+class MenuScene:
+    def __init__(self, game):
+        self.game = game
+        self.screen = game.screen
+        self.background = pygame.transform.scale(
+            pygame.image.load(ASSET_PATH + "home_background.png"), (WIDTH, HEIGHT)
+        )
