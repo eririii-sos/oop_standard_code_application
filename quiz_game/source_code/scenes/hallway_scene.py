e6b3fe90edcd725972b9fd4b48fee59e3e608cd5
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-07-08 00:07:39 +0800

    Loaded character facial expression set

--- quiz_game/source_code/scenes/hallway_scene.py
+++ quiz_game/source_code/scenes/hallway_scene.py
@@ -11,3 +11,14 @@ class HallwayScene:
         self.background = pygame.transform.scale(
             pygame.image.load(ASSET_PATH + "school_hall_background.jpg"), (WIDTH, HEIGHT)
         )
+
+        # Character facial expression set
+        self.char_expression_1 = pygame.transform.scale(pygame.image.load(ASSET_PATH + "neutral_face_2.png"), (300, 400))
+        self.char_expression_2 = pygame.transform.scale(pygame.image.load(ASSET_PATH + "shocked_face.png"), (300, 400))
+        self.char_expression_3 = pygame.transform.scale(pygame.image.load(ASSET_PATH + "disbelief_face.png"), (300, 400))
+        self.char_expression_4 = pygame.transform.scale(pygame.image.load(ASSET_PATH + "wondering_face.png"), (300, 400))
+        self.char_expression_5 = pygame.transform.scale(pygame.image.load(ASSET_PATH + "thinking_face_2.png"), (300, 400))
+        self.char_expression_6 = pygame.transform.scale(pygame.image.load(ASSET_PATH + "plotting_face.png"), (300, 400))
+        self.char_expression_7 = pygame.transform.scale(pygame.image.load(ASSET_PATH + "begging_face.png"), (300, 400))
+        self.char_expression_8 = pygame.transform.scale(pygame.image.load(ASSET_PATH + "begging_face_2.png"), (300, 400))
+        self.char_expression_9 = pygame.transform.scale(pygame.image.load(ASSET_PATH + "thinking_face.png"), (300, 400))
