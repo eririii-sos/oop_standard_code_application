c9e0007a275b93c03051f6a947a384c1c170bcf1
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-07-08 22:21:24 +0800

    Declared new class + Initialized objects

--- quiz_game/source_code/scenes/quiz_scene.py
+++ quiz_game/source_code/scenes/quiz_scene.py
@@ -4,3 +4,8 @@ import os
 from settings import ASSET_PATH
 
 QUIZ_FILE_PATH = os.path.join(ASSET_PATH, "quiz_creator_questions.txt")
+
+class QuizScene:
+    def __init__(self, game):
+        self.game = game
+        self.screen = game.screen
