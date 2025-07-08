817085d2549c63c6a0724d655e4a95e1100723ba
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-07-08 22:24:41 +0800

    Imported necessities + Set font

--- quiz_game/source_code/scenes/quiz_scene.py
+++ quiz_game/source_code/scenes/quiz_scene.py
@@ -1,7 +1,8 @@
 # scenes/quiz_scene.py
 
+import pygame
 import os
-from settings import ASSET_PATH
+from settings import ASSET_PATH, QUIZ_FONT_SIZE
 
 QUIZ_FILE_PATH = os.path.join(ASSET_PATH, "quiz_creator_questions.txt")
 
@@ -9,3 +10,4 @@ class QuizScene:
     def __init__(self, game):
         self.game = game
         self.screen = game.screen
+        self.font = pygame.font.Font(None, QUIZ_FONT_SIZE)
