dc814690aed20400a483a80d85f21eb028278b2d
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-07-08 23:45:51 +0800

    Imported necessities + set font

--- quiz_game/source_code/scenes/result_scene.py
+++ quiz_game/source_code/scenes/result_scene.py
@@ -1,6 +1,10 @@
 # scenes/result_scene.py
 
+import pygame
+from settings import QUIZ_FONT_SIZE
+
 class ResultScene:
     def __init__(self, game):
         self.game = game
         self.screen = game.screen
+        self.font = pygame.font.Font(None, QUIZ_FONT_SIZE)
