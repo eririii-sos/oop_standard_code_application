df5dac05df0f73482c46c23a706958305fa1340a
Jacey Concepcion <jacey.concepcion06@gmail.com>
2025-07-08 23:48:16 +0800

    Set title font, Initialized objects

--- quiz_game/source_code/scenes/result_scene.py
+++ quiz_game/source_code/scenes/result_scene.py
@@ -4,7 +4,11 @@ import pygame
 from settings import QUIZ_FONT_SIZE
 
 class ResultScene:
-    def __init__(self, game):
+    def __init__(self, game, user_score, total_questions):
         self.game = game
         self.screen = game.screen
         self.font = pygame.font.Font(None, QUIZ_FONT_SIZE)
+        self.title_font = pygame.font.Font(None, QUIZ_FONT_SIZE + 16)
+
+        self.user_score = user_score
+        self.total_questions = total_questions
